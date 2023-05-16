72ffd55297566c6e6252f8180317ddd6335df4cd
krish200012 <76152985+krish200012@users.noreply.github.com>
2023-05-16 23:42:29 +0530

    Create outletRoutes.js

---
+++ outletRoutes.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const { getOutlets, createOutlet, updateOutlet, deleteOutlet, getOne } = require('../controllers/outletsController');
+const router=express.Router();
+
+router.route("/get").get(getOutlets);
+router.route("/create").post(createOutlet)
+router.route("/update/:id").patch(updateOutlet)
+router.route("/delete/:id").delete(deleteOutlet)
+router.route("/getone/:id").get(getOne)
+
+module.exports=router
